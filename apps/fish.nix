0c31bfd8d66b62489891110b8d0de372dd5b4643
godsmith <jgodfrey-smith@pm.me>
2023-05-28 14:51:30 -0400

    Updated fish config

--- apps/fish.nix
+++ apps/fish.nix
@@ -1,6 +1,11 @@
 {
   programs.fish = {
     enable = true;
+    interactiveShellInit = "
+      fish_vi_key_bindings
+      fish_add_path -p ~/.cargo/bin/
+      fish_add_path -p ~/.local/bin/
+      zoxide init fish | source";
     shellAliases = {
       ll = "exa -alF";
       nv = "nvim";
