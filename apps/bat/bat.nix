c4b0b4ddaa28123bebf627e6cf6c9080b7ca2f81
godsmith <jgodfrey-smith@pm.me>
2023-05-31 16:29:32 -0400

    Updated bat config

--- apps/bat/bat.nix
+++ apps/bat/bat.nix
@@ -1,8 +1,16 @@
+{ pkgs, ... }:
+
 {
   programs.bat = {
     enable = true;
     config = {
         theme = "Dracula";
     };
+    # TODO: add when extraPackages is available
+    # extraPackages = with pkgs.bat-extras; [
+    #   batdiff # git diff
+    #   batgrep # grep replacement
+    #   batman # manual highlighter
+    # ];
   };
 }
